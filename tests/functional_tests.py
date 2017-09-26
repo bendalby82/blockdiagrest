3980c1e4ba19292e236d4f5eaa0c6141d2cc89e3
bendalby82 <bdalby@pivotal.io>
2017-09-26 23:01:22 +0100

    Added functional tests

---
+++ tests/functional_tests.py
@@ -0,0 +1,22 @@
+# Virendra goes to VMap.com and logs in
+# He sees his list of current VMaps
+# He decides to create a new VMap, complete with the stages he wants it to have
+# Virendra now navigates to his new VMap, and is able to enter some values for a Run
+# Once he submits his values, he can see a VMap with timings, a Process Cycle Time
+# calculation, for that run
+# He can also enter another run
+# He can also see all runs entered so far
+# He can also see a VMap with timings and a Process Cycle Time calculatio for
+# all runs entered
+# When he is satisfied, he can log out
+# 
+#  
+# 
+# 
+# 
+#  
+# 
+# 
+# 
+# 
+# 
